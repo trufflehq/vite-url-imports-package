43bb742e3cf1edc31bbe3509cffe325a29a4a4c4
Austin Hallock <austinhallock@gmail.com>
2022-07-07 11:42:25 -0600

    vite with url imports as npm installable package

---
+++ src/node/server/__tests__/search-root.spec.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path'
+import { searchForWorkspaceRoot } from '../searchRoot'
+
+describe('searchForWorkspaceRoot', () => {
+  test('lerna', () => {
+    const resolved = searchForWorkspaceRoot(
+      resolve(__dirname, 'fixtures/lerna/nested')
+    )
+    expect(resolved).toBe(resolve(__dirname, 'fixtures/lerna'))
+  })
+
+  test('pnpm', () => {
+    const resolved = searchForWorkspaceRoot(
+      resolve(__dirname, 'fixtures/pnpm/nested')
+    )
+    expect(resolved).toBe(resolve(__dirname, 'fixtures/pnpm'))
+  })
+
+  test('yarn', () => {
+    const resolved = searchForWorkspaceRoot(
+      resolve(__dirname, 'fixtures/yarn/nested')
+    )
+    expect(resolved).toBe(resolve(__dirname, 'fixtures/yarn'))
+  })
+
+  test('yarn at root', () => {
+    const resolved = searchForWorkspaceRoot(resolve(__dirname, 'fixtures/yarn'))
+    expect(resolved).toBe(resolve(__dirname, 'fixtures/yarn'))
+  })
+
+  test('none', () => {
+    const resolved = searchForWorkspaceRoot(
+      resolve(__dirname, 'fixtures/none/nested')
+    )
+    // resolved to vite repo's root
+    expect(resolved).toBe(resolve(__dirname, '../../../../../..'))
+  })
+})
